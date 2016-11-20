3620ae8ef836aa41715e2ec1517ae545879cee6b
Pavlos Vos <pavlos@vos.gr>
2016-11-20 15:20:00 +0100

    WORKING NPM INSTALL server locally

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -21,7 +21,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className={cx(s.root, this.props.className)} role="navigation">
-        <Link className={s.link} to="/about">About</Link>
+        <Link className={s.link} to="/about">About test</Link>
         <Link className={s.link} to="/contact">Contact</Link>
         <span className={s.spacer}> | </span>
         <Link className={s.link} to="/login">Log in</Link>
